1ac8ca492c1701f0667a3667a3f62c2db06c2bf3
jonayet-nur <jonayetnur@gmail.com>
2026-03-06 05:08:53 +0600

    empty lesson design create

--- script/index.js
+++ script/index.js
@@ -8,7 +8,9 @@ const loadLevelWord= (id)=>{
 const url= (`https://openapi.programming-hero.com/api/level/${id}`)
 fetch(url)
 .then(res => res.json())
-.then(data => displayLevelWord(data.data))
+.then(data => {
+    displayLevelWord(data.data)
+})
 }
 
 // {
@@ -22,15 +24,26 @@ fetch(url)
 const displayLevelWord = (words)=>{
     const wordContainer = document.getElementById("word-container")
     wordContainer.innerHTML = ""
+// ata use jokon lesson4 and lesson7 oi gular modde kichu nai tai akta card dekabu
+    if(words.length == 0){
+        wordContainer.innerHTML = `
+
+         <div class="text-center col-span-full space-y-3 font-bangla">
+             <img src="assets/alert-error.png" alt="" class=" mx-auto">
+            <p class="text-sm text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="text-3xl font-medium ">নেক্সট Lesson এ যান</h2>
+        </div>
+        `
+    }
 
     words.forEach((word) => {
         // console.log(word);
         const card = document.createElement("div")
         card.innerHTML =` 
-        <div class="bg-white rounded-xl shadow-md text-center py-10 px-5 space-y-4">
-            <h2 class="text-3xl font-bold">${word.word}</h2>
+        <div class="bg-white rounded-xl shadow-md text-center py-10 px-5 space-y-4 w-full">
+            <h2 class="text-3xl font-bold">${word.word ? word.word:"শব্দ  পাওয়া যায়নি"}</h2>
             <p class="font-medium">Meaning /Pronounciation</p>
-            <div class="text-2xl font-medium font-bangla">${word.meaning}/ ${word.pronunciation}</div>
+            <div class="text-xl font-medium font-bangla">${word.meaning ? word.meaning:"অর্থ পাওয়া যায়নি"}/ ${word.pronunciation}</div>
             <div class="flex justify-between items-center ">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
