85054e5b40e73a27ab1a3c0af75aab66579e2551
jonayet-nur <jonayetnur@gmail.com>
2026-03-05 12:15:11 +0600

    all lesson button create in js with dynamically

--- script/index.js
+++ script/index.js
@@ -1,1 +1,24 @@
-console.log("hello I am js")
+const loadLessons = ()=>{
+    fetch('https://openapi.programming-hero.com/api/levels/all') //promise of response
+    .then((res) => res.json()) //promise of json data
+    .then((json) => displayLesson(json.data)) //data ata  api all property name
+}
+const displayLesson = (lessons)=>{
+// 1.get the container and empty
+const levelContainer = document.getElementById("level-container")
+levelContainer.innerHTML = ""
+
+// 2.get into every lessons
+for(let lesson of lessons){
+
+    //     3.create Element
+    const btnDiv = document.createElement("div")
+    btnDiv.innerHTML = `
+    <button  class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson-${lesson.level_no}</button>
+     `
+//     4.appent into container
+levelContainer.appendChild(btnDiv);
+}
+
+}
+loadLessons()
